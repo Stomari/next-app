338db2ac4885c3eb3ea0551a1ec229870ba58245
Sergio <sfestatomari@gmail.com>
2023-11-06 18:21:20 -0300

    add background image

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,22 +1,31 @@
+import { ReactNode } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { ReactNode } from "react";
+
+import beerImg from "@/public/beer.png";
 
 export default function Layout({
   children,
   home,
 }: {
-  children: ReactNode;
+  children?: ReactNode;
   home?: boolean;
 }) {
   return (
-    <div>
+    <div
+      className="h-screen"
+      style={{
+        backgroundImage: `url(${beerImg.src})`,
+        backgroundSize: 200,
+        backgroundPositionY: 50,
+      }}
+    >
       <Head>
         <title>Beer App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="w-full bg-gradient-to-t from-yellow-400 to-white p-2 flex justify-between items-center">
+      <header className="fixed w-full bg-gradient-to-t from-yellow-400 to-white p-2 flex justify-between items-center border-b-2 border-black">
         <h1 className="font-bold text-lg w-fit">Beer App</h1>
         {!home && (
           <Link className="text-blue-700" href={"/"}>
@@ -25,7 +34,7 @@ export default function Layout({
         )}
       </header>
 
-      <div>{children}</div>
+      <main className="pt-10">{children}</main>
     </div>
   );
 }

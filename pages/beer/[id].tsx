70fa12b802e2e60f599487e77d42db15c783f2a1
Sergio <sfestatomari@gmail.com>
2023-11-06 18:21:44 -0300

    add beer page

---
+++ pages/beer/[id].tsx
@@ -0,0 +1,31 @@
+import { getBeerData } from "@/api";
+import Layout from "@/components/layout";
+import { IBeer } from "@/types/api";
+import { GetStaticPaths, GetStaticProps } from "next";
+
+interface IBeerProps {
+  beerData: IBeer[];
+}
+
+export const Beer = ({ beerData }: IBeerProps) => {
+  const { name } = beerData[0];
+  return (
+    <Layout>
+      <h1>{name}</h1>
+    </Layout>
+  );
+};
+
+export default Beer;
+
+export const getStaticPaths: GetStaticPaths = async (props) => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const beerData = await getBeerData(parseInt(params?.id as string));
+  return { props: { beerData } };
+};
--- pages/beers/index.tsx
+++
@@ -1,5 +0,0 @@
-export const Beers = () => {
-  return <main></main>;
-};
-
-export default Beers;

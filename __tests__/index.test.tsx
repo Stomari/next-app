5c9f3f165770eebb1771ef84ddc00eb8fc659f3a
Sergio <sfestatomari@gmail.com>
2023-11-13 11:07:11 -0300

    fix tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,21 +1,21 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { IBeer } from "@/types/api";
-import Home from "../pages/index";
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { IBeer } from '@/types/api';
+import Home from '../pages/index';
 
-describe("Home", () => {
-  it("renders title", () => {
+describe('Home', () => {
+  it('renders title', () => {
     render(<Home />);
 
-    const title = screen.getByTestId("titlee");
+    const title = screen.getByTestId('title');
 
     expect(title).toBeInTheDocument();
   });
 
-  it("renders beers list", () => {
+  it('renders beers list', () => {
     const beersListMock: IBeer[] = [
-      { id: 1, name: "Beer 1", description: "Tasty beer", image_url: "url" },
-      { id: 2, name: "Beer 2", description: "Another tasty beer" },
+      { id: 1, name: 'Beer 1', description: 'Tasty beer', image_url: 'url' },
+      { id: 2, name: 'Beer 2', description: 'Another tasty beer' },
     ];
 
     render(<Home beers={beersListMock} />);
